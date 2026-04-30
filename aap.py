b423e29bd4514d78e31e5143ff92922df259b43a
Arykav-1830 <dixitaryan107@gmail.com>
2026-04-30 23:14:33 +0530

    Update aap.py

--- aap.py
+++ aap.py
@@ -1,18 +1,27 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
 model = pickle.load(open('model.pkl', 'rb'))
+columns = pickle.load(open('columns.pkl', 'rb'))
 
 st.title("Customer Churn Prediction")
 
-feature1 = st.number_input("Enter Feature 1")
-feature2 = st.number_input("Enter Feature 2")
+
+input_df = pd.DataFrame(columns=columns)
+
+
+input_df.loc[0, 'Feature1'] = st.number_input("Feature1")
+input_df.loc[0, 'Feature2'] = st.number_input("Feature2")
+
+
+input_df = input_df.fillna(0)
 
 if st.button("Predict"):
-    result = model.predict([[feature1, feature2]])
-    
-    if result == 1:
+    result = model.predict(input_df)
+
+    if result[0] == 1:
         st.error("Customer will leave ❌")
     else:
         st.success("Customer will stay ✅")
